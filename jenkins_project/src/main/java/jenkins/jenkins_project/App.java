7b6836c7dd1fb124733c6b2aaee4728c6f974038
Monika1266 <monikasadhasivam766@gmail.com>
2024-05-28 10:50:16 +0530

    First commit

---
+++ jenkins_project/src/main/java/jenkins/jenkins_project/App.java
@@ -0,0 +1,13 @@
+package jenkins.jenkins_project;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
